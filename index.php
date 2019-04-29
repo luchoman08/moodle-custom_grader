9506a2572d8aff20dcaccb4f902e2b5a9617f313
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2019-04-29 10:36:35 -0500

    fix: font awesome crash in moove theme

--- index.php
+++ index.php
@@ -2,8 +2,6 @@
 require_once '../../config.php';
 require_once (__DIR__ .'/managers/grader_lib.php');
 $courseid        = required_param('id', PARAM_INT);
-
-
 $PAGE->requires->jquery();
 
 $url = new moodle_url('/local/customgrader/index.php', array('id' => $courseid));
@@ -19,7 +17,11 @@ $context = context_course::instance($course->id);
 require_capability('moodle/grade:manage', $context);
 $PAGE->requires->jquery_plugin('ui');
 $PAGE->requires->css('/local/customgrader/style/vue-js-modal.css');
-$PAGE->requires->css('/local/customgrader/style/font-awesome.css');
+// Moove have his own font awesome, if is required hear all icons crash
+if(!$PAGE->theme->name === 'moove') {
+    $PAGE->requires->css('/local/customgrader/style/font-awesome.css');
+}
+
 $PAGE->requires->css('/local/customgrader/style/vue-flex.css', true);
 $PAGE->requires->css('/local/customgrader/style/vue-toasted.css', true);
 $PAGE->requires->css('/local/customgrader/style/styles_grader.css', true);
