ace082ff3f0bde83b04711412c638bca116a4a5e
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2019-06-06 18:50:55 -0500

    added author to files

--- classes/custom_grade_category.php
+++ classes/custom_grade_category.php
@@ -1,11 +1,37 @@
 <?php
 namespace local_customgrader;
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Custom grader report for ASES utitlities
+ *
+ * @author     Luis Gerardo Manrique Cardona
+ * @package    block_ases
+ * @copyright  2018 Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * 
+ */
 class custom_grade_category extends \grade_category {
     /**
      * @var number $grade_item Associated grade item id
      */
     public $grade_item;
-
+    /**
+     * Add the respective grade item belonging to grade category
+     */
     private static function append_grade_item(\grade_category $grade_category) {
         $_grade_category = (object) $grade_category;
         $item = $grade_category->get_grade_item();
